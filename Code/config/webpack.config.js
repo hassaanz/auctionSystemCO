b6e5467b0312f7fa35c42fb580d0186b353bc224
Hassaan Zaidi <hassaanz@gmail.com>
2016-04-17 04:33:54 +0500

    Added ui-router and index file

--- Code/config/webpack.config.js
+++ Code/config/webpack.config.js
@@ -3,17 +3,41 @@ var path = require('path');
 var nodeModules = path.resolve(__dirname, '../node_modules');
 var pathToAngular = path.resolve(nodeModules, 'angular/angular.min.js');
 
-module.exports = {
+var deps = [
+  'angular/angular.min.js',
+  'moment/min/moment.min.js',
+  'underscore/underscore-min.js',
+];
+
+var config = {
     entry: ['webpack/hot/dev-server', path.resolve(__dirname, '../client/app.js')],
+    resolve: {
+        // to simply require('file') instead of require('file.coffee')
+        extensions: ['', '.js', '.json', '.coffee'],
+        alias: {}
+    },
     output: {
         path: path.resolve(__dirname, '../build'),
         filename: 'bundle.js'       
     },
     module: {
         loaders: [
-            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
+            // js-babel loader for ES5 awesomeness
+            { test: /\.js$/, loader: 'babel'},
+            // html / ng-template loader
+            {test: /\.html$/, loader: 'ngtemplate!html'},
+            // css, less, sass loaders
+            { test: /\.sass$/, loader: 'style!css!sass' },
+            { test: /\.less$/, loader: 'style!css!less' }, // use ! to chain loaders
+            { test: /\.css$/, loader: 'style!css' },
+            // image loaders
+            { test: /\.(png|jpg)$/, loader: 'url?limit=25000' },
+            // Font loaders
+            { test: /\.woff$/, loader: 'url?limit=100000'},
+            { test: /\.woff2$/, loader: 'url?limit=100000'},
+            { test: /\.ttf$/, loader: 'url?limit=100000'},
+            { test: /\.eot$/, loader: 'file'},
+            { test: /\.svg$/, loader: 'url?limit=100000'}
         ],
         noParse: [pathToAngular]
     },
@@ -22,11 +46,17 @@ module.exports = {
     //         'angular': 'angular'
     //     })
     // ],
-    resolve: {
-    // to simply require('file') instead of require('file.coffee')
-        extensions: ['', '.js', '.json', '.coffee'],
-        alias: {
-            'angular': pathToAngular
-        }
-  }
+    
 };
+
+// Run through deps and extract the first part of the path, 
+// as that is what you use to require the actual node modules 
+// in your code. Then use the complete path to point to the correct
+// file and make sure webpack does not try to parse it
+deps.forEach(function (dep) {
+    var depPath = path.resolve(nodeModulesDir, dep);
+    config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+    config.module.noParse.push(depPath);
+});
+
+module.exports = config;
